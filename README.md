9ff4de5144fc611c1a0c6b01777b8e411836b2a4
Guillermo Q. Chavez <gchavez@infonet.com.gt>
2019-11-01 15:57:35 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## euphonic-sessions-with-kyau-and-albert-playlist-prettifier
+Take a playlist from Euphonic Sessions with Kyay & Albert episode (from soundcloud) and output formated playlist.
+
+### How to use?
+
+ 1. Clone the repository or download a [zip version](https://github.com/chepe263/euphonic-sessions-with-kyau-and-albert-playlist-pretifier/archive/master.zip)
+ 2. Obtain a playlist from an episode on [Soundcloud](https://soundcloud.com/euphonicsessions)
+ 3. Create a text file named `input.txt` on the same place where `index.js` is located.
+ 4. Inside `input.txt` paste the unformatted playlist.
+ 5. Open a command prompt in the same directory execute `npm start`
+ 6. Open the newly created `output.txt` with the formatted playlist.
---
+++ index.js
@@ -0,0 +1,41 @@
+﻿const fs = require("fs");
+
+var contents = fs.readFileSync('input.txt', 'utf8');
+var playlist_items = contents.split("\r\n");
+var found_close_parenthesis = false;
+var track_of_the_month = -1;
+for(let i= 0; i < playlist_items.length; i++){
+	found_close_parenthesis = false;
+	playlist_items[i] = playlist_items[i].substring(0, 2) + "." + playlist_items[i].substring(2);
+	playlist_items[i] = playlist_items[i].replace(" - ", " - \"");
+	//"Track of The Month"! 
+	if(track_of_the_month == -1 && !!playlist_items[i].match("TRACK OF THE MONTH")){
+		track_of_the_month = i;
+		playlist_items[i] = playlist_items[i].replace("[TRACK OF THE MONTH]", "");
+	}
+	
+	if( playlist_items[i].match(/\(/gi) ){
+		// Encontre (		
+		let partes = playlist_items[i].split(' (')//.reverse()
+		for(e = 0; e < partes.length; e++){
+			if(partes[e].match(/\)/gi)){
+				if(!found_close_parenthesis){
+					partes[e] = "\" (" + partes[e];					
+					found_close_parenthesis = true;
+				} else {
+					partes[e] = "(" + partes[e];
+				}
+			}
+		}
+		//playlist_items[i] = partes.reverse().join("");
+		playlist_items[i] = partes.join("");
+	}
+	if(!found_close_parenthesis){
+		playlist_items[i] = playlist_items[i].replace(" [", "\" [");
+	}
+	playlist_items[i] = playlist_items[i].replace(")(", ") (");
+}
+if(track_of_the_month !== -1){
+	playlist_items[track_of_the_month] = "\r\n" + "[TRACK OF THE MONTH]" + "\r\n" + playlist_items[track_of_the_month] + "\r\n";
+}
+fs.writeFileSync('output.txt', playlist_items.join("\r\n"));
